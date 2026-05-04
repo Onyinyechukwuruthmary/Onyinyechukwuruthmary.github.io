8c05802a53221c4f98d4e7efdebd624c4643fc5e
Onyinyechukwuruthmary <sophia1995ruth@gmail.com>
2026-05-04 11:20:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Simple interaction example
 
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".btn");
+
+  button.addEventListener("click", function () {
+    alert("You are being redirected to LinkedIn!");
+  });
+});
